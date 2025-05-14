6d21b3140c68da814cc5ac7c0ad6c74b6f5c403d
Kamogelo <ekmogasoa@gmail.com>
2025-05-14 15:03:12 +0200

    Python inheritance

--- OOP.py
+++ OOP.py
@@ -4,6 +4,7 @@
 
 username = str(input("what is your name?"))
 userage = int(input("what is your age?"))
+enrollmentYear = int(input("What year did you enroll?"))
 
 class Person:
     def __init__ (self, name, age):
@@ -20,3 +21,20 @@ class Person:
 user1 = Person(username, userage)
 user1.user()
 user1.checkUserAge()
+
+# Python Inheritance
+# Inheritance allows us to define a class that inherits all the methods and properties from another class.
+# Parent class is the class being inherited from, also called base class.
+# Child class is the class that inherits from another class, also called derived class.
+
+class Student(Person):
+    def __init__(self, name, age, enrolled):
+        super().__init__(name, age)
+        self.enrolled = enrolled
+
+    def enrolledYear (self):
+        return f"Hello {self.name} you have enrolled in this colleage since {self.enrolled}"
+
+Student1 = Student(username, userage , enrollmentYear)
+print(Student1.enrolledYear())
+

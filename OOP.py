3c7100a930b2964aff0173393ff8a51ede5746d8
Kamogelo <ekmogasoa@gmail.com>
2025-05-14 16:36:26 +0200

    chacked age logic

--- OOP.py
+++ OOP.py
@@ -1,5 +1,3 @@
-import moduleConcept
-
 # Python is an object oriented programming language.
 # Almost everything in Python is an object, with its properties and methods.
 # A Class is like an object constructor, or a "blueprint" for creating objects
@@ -17,7 +15,7 @@ class Person:
         print(f"Hello {self.name} you are {self.age} years old.")
 
     def checkUserAge (self):
-        if(userage < 18):
+        if(self.age < 18):
             print(f"hello {self.name} i can't allow you to enter your to young {self.age} ")
 
 user1 = Person(username, userage)
